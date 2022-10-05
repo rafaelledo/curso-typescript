a282809127f6c183d672fc3f2b3bad4bf6757ec6
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-10-04 21:04:15 -0300

    first commit

---
+++ types/types.js
@@ -0,0 +1,19 @@
+"use strict";
+// string
+let personName = 'João';
+console.log(personName);
+// personName = 28
+// numbers
+let age = 27;
+// age = 'Ana'
+age = 27.5;
+console.log(age);
+// boolean
+let haveHobbies = false;
+// haveHobbies = 1
+console.log(haveHobbies);
+// explicit types
+let myAge;
+myAge = 27;
+console.log(typeof myAge);
+// myAge = '27'
---
+++ types/types.ts
@@ -0,0 +1,21 @@
+// string
+let personName: string = 'João'
+console.log(personName)
+// personName = 28
+
+// numbers
+let age: number = 27
+// age = 'Ana'
+age = 27.5
+console.log(age)
+
+// boolean
+let haveHobbies: boolean = false
+// haveHobbies = 1
+console.log(haveHobbies)
+
+// explicit types
+let myAge: number
+myAge = 27
+console.log(typeof myAge)
+// myAge = '27'
